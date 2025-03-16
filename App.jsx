058bd7b059eb903fc6c5f5e6d9765a3b582ffcf0
Shaful Azim <shafiulmizbah2@gmail.com>
2025-03-17 05:32:14 +0600

    feat: removed download all feature in initial screen and update as new UI

--- App.jsx
+++ App.jsx
@@ -1,46 +1,61 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from 'screens/home';
-import FavoriteScreen from 'screens/favorite';
-import DetailsScreen from 'screens/details';
-import TabBar from 'components/tab-bar';
-import ListHeader from 'components/list-header';
-import DetailsHeader from 'screens/details/header';
-import Downloading from 'components/downloading';
-import { useContext } from 'react';
-import { StoreContext } from '@/services/store';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "screens/home";
+import FavoriteScreen from "screens/favorite";
+import DetailsScreen from "screens/details";
+import TabBar from "components/tab-bar";
+import ListHeader from "components/list-header";
+import DetailsHeader from "screens/details/header";
+import { useContext, useEffect } from "react";
+import { StoreContext } from "@/services/store";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 const TabNavigation = () => {
   return (
-    <Tab.Navigator tabBar={(props) => <TabBar {...props}/>}>
-      <Tab.Screen name="Home" component={HomeScreen} options={{
-        headerShown: false,
-      }}/>
-      <Tab.Screen name="Favorite" component={FavoriteScreen} options={{
-        headerShown: false,
-      }}/>
+    <Tab.Navigator tabBar={(props) => <TabBar {...props} />}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Favorite"
+        component={FavoriteScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
     </Tab.Navigator>
   );
 };
 
 export default function App() {
-  const { downloading } = useContext(StoreContext);
+  const { fetchRemoteData } = useContext(StoreContext);
 
-  if( downloading.state ) {
-    return <Downloading />
-  }
+  useEffect(() => {
+    fetchRemoteData();
+  }, []);
 
   return (
     <Stack.Navigator>
-      <Stack.Screen name="List" component={TabNavigation} options={{
-        header: ListHeader,
-      }}/>
-      <Stack.Screen name="Details" component={DetailsScreen} options={{
-        header: DetailsHeader,
-      }}/>
+      <Stack.Screen
+        name="List"
+        component={TabNavigation}
+        options={{
+          header: ListHeader,
+        }}
+      />
+      <Stack.Screen
+        name="Details"
+        component={DetailsScreen}
+        options={{
+          header: DetailsHeader,
+        }}
+      />
     </Stack.Navigator>
   );
 }
--- components/audio-list/index.jsx
+++ components/audio-list/index.jsx
@@ -1,78 +1,167 @@
-import { useContext, useMemo } from 'react';
-import { View, Text, Pressable, Dimensions } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { heading } from 'constants/styles';
-import Heart from '@/components/heart';
-import { StoreContext } from '@/services/store';
-import styles from './style';
-import Play from '../play';
-import { convertMilliseconds } from 'helpers/utils';
+import { useContext, useEffect, useMemo, useState } from "react";
+import { View, Text, Pressable, Dimensions, StyleSheet } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import { heading } from "constants/styles";
+import Heart from "@/components/heart";
+import { StoreContext } from "@/services/store";
+import styles from "./style";
+import Play from "../play";
+import { convertMilliseconds } from "helpers/utils";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import colors from "constants/colors";
+import * as FileSystem from "expo-file-system";
 
-export default function AudioList( props ) {
-    const navigation = useNavigation();
-    const { title, isFavoriteOnly } = props;
-    const { data, isFavoriteById, toggleFavorite, player, sound, currentTime, favorite, search, setSound, currentAudio } = useContext(StoreContext);
+const ASSET_FOLDER = FileSystem.documentDirectory + "audio/";
 
-    const onPress = async (id) => {
-        if( sound.id == id && currentAudio ) {
-            return navigation.navigate('Details');
+export default function AudioList(props) {
+  const navigation = useNavigation();
+  const { title, isFavoriteOnly } = props;
+  const {
+    data,
+    isFavoriteById,
+    toggleFavorite,
+    player,
+    sound,
+    currentTime,
+    favorite,
+    search,
+    setSound,
+    currentAudio,
+    downloadAudio,
+    downloadAllAudios,
+    downloadingItem,
+    downloadingAll,
+    setDownloadingItem,
+  } = useContext(StoreContext);
+  const [downloadedTracks, setDownloadedTracks] = useState([]);
+
+  useEffect(() => {
+    const fetchDownloadedTracks = async () => {
+      const storedTracks = await AsyncStorage.getItem("audio_meta");
+      const downloadedTracks = storedTracks ? JSON.parse(storedTracks) : [];
+
+      const downloadedTrackIds = [];
+      for (const track of downloadedTracks) {
+        const audioPath = ASSET_FOLDER + track.id + ".mp3";
+
+        const fileInfo = await FileSystem.getInfoAsync(audioPath);
+
+        if (fileInfo.exists) {
+          downloadedTrackIds.push(track.id);
         }
+      }
 
-        setSound(id)
-        navigation.navigate('Details');
+      setDownloadedTracks(downloadedTrackIds);
+    };
+    fetchDownloadedTracks();
+  }, [downloadingItem]);
+
+  const onPress = async (id) => {
+    if (sound.id == id && currentAudio) {
+      return navigation.navigate("Details");
     }
 
-    const beforeFilter = useMemo(() => {
-        if( isFavoriteOnly ) {
-            return data.filter(item => (favorite || []).includes(item.id));
-        } else {
-            return data;
-        }
-    } , [data, favorite, isFavoriteOnly]);
+    setSound(id);
+    navigation.navigate("Details");
+  };
 
-    const filteredData = useMemo(() => {
-        return beforeFilter.filter(item => item.title.toLowerCase().includes(search.query.toLowerCase()));
-    }, [data, search.query, favorite])
+  const beforeFilter = useMemo(() => {
+    if (isFavoriteOnly) {
+      return data.filter((item) => (favorite || []).includes(item.id));
+    } else {
+      return data;
+    }
+  }, [data, favorite, isFavoriteOnly]);
 
-    const isLastIndex = (index) => index === filteredData.length - 1;
-    const isCurrentTrack = (id) => sound.id === id && player.isPlaying;
+  const filteredData = useMemo(() => {
+    return beforeFilter.filter((item) =>
+      item?.title?.toLowerCase()?.includes(search?.query?.toLowerCase())
+    );
+  }, [data, search.query, favorite]);
 
-    return (
-        <View style={styles.wrapper}>
-            <Text style={heading.lg}>{search.isFocused ? 'Search result' : title}</Text>
-            <View style={styles.list}>
-                { filteredData.length > 0 ? filteredData.map((item, index) => (
-                    <View key={item.id} style={{
-                        ...styles.item,
-                        marginBottom: isLastIndex(index) ? 15 : 0
-                    }}>
-                        <Pressable style={styles.button} onPress={() => onPress(item.id)}>
-                            <Play isActive={isCurrentTrack(item.id)}/>
-                            <View>
-                                <Text style={heading.sm}>{item.title}</Text>
-                                <View style={{ flexDirection: 'row' }}>
-                                    {
-                                        isCurrentTrack(item.id) && <>
-                                            <Text style={[styles.duration, styles.activeDuration]}>{convertMilliseconds(currentTime)}</Text>
-                                            <Text style={styles.duration}> / </Text>
-                                        </>
-                                    }
-                                    <Text style={styles.duration}>{convertMilliseconds(item.duration)}</Text>
-                                </View>
-                            </View>
-                        </Pressable>
-                        <Pressable onPress={() => toggleFavorite(item.id)}>
-                            <Heart isActive={isFavoriteById(item.id)} isFill={true}/>
-                        </Pressable>
-                    </View> 
-                )) : <Text style={{
-                    textAlign: 'center',
-                    marginTop: Dimensions.get('window').height / 4,
-                    color: '#D6D6D6',
-                    fontSize: 16,
-                    fontFamily: 'Satoshi-Bold'
-                }}>No result found :(</Text> }
+  const isLastIndex = (index) => index === filteredData.length - 1;
+  const isCurrentTrack = (id) => sound.id === id && player.isPlaying;
+  const isDownloaded = (id) => downloadedTracks.includes(id);
+  const isDownloading = (id) => downloadingItem === id;
+
+  return (
+    <View style={styles.wrapper}>
+      <View style={styles.title_wrapper}>
+        <Text style={heading.lg}>
+          {search.isFocused ? "Search result" : title}
+        </Text>
+        {downloadingAll ? (
+          <AntDesign name="loading1" size={24} color={colors.primary} />
+        ) : (
+          <Pressable onPress={downloadAllAudios}>
+            <Text style={styles.download_all}>Download all</Text>
+          </Pressable>
+        )}
+      </View>
+
+      <View style={styles.list}>
+        {filteredData.length > 0 ? (
+          filteredData.map((item, index) => (
+            <View
+              key={`${item.id}${item.title.split(" ").join("")}`}
+              style={{
+                ...styles.item,
+                marginBottom: isLastIndex(index) ? 15 : 0,
+              }}
+            >
+              <Pressable style={styles.button} onPress={() => onPress(item.id)}>
+                <Play isActive={isCurrentTrack(item.id)} />
+                <View>
+                  <Text style={heading.sm}>{item.title}</Text>
+                  <View style={{ flexDirection: "row" }}>
+                    {isCurrentTrack(item.id) && (
+                      <>
+                        <Text style={[styles.duration, styles.activeDuration]}>
+                          {convertMilliseconds(currentTime)}
+                        </Text>
+                        <Text style={styles.duration}> / </Text>
+                      </>
+                    )}
+                    <Text style={styles.duration}>
+                      {convertMilliseconds(item.duration)}
+                    </Text>
+                  </View>
+                </View>
+              </Pressable>
+              {isDownloading(item.id) ? (
+                <AntDesign name="loading1" size={24} color={colors.primary} />
+              ) : item?.downloaded ? (
+                <Pressable onPress={() => toggleFavorite(item.id)}>
+                  <Heart isActive={isFavoriteById(item.id)} isFill={true} />
+                </Pressable>
+              ) : (
+                <Pressable
+                  onPress={async () => {
+                    setDownloadingItem(item.id);
+                    await downloadAudio(item);
+                    setDownloadingItem(null);
+                  }}
+                >
+                  <AntDesign name="download" size={24} color={colors.primary} />
+                </Pressable>
+              )}
             </View>
-        </View>
-    )
+          ))
+        ) : (
+          <Text
+            style={{
+              textAlign: "center",
+              marginTop: Dimensions.get("window").height / 4,
+              color: "#D6D6D6",
+              fontSize: 16,
+              fontFamily: "Satoshi-Bold",
+            }}
+          >
+            No result found :(
+          </Text>
+        )}
+      </View>
+    </View>
+  );
 }
